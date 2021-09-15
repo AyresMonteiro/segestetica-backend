e11e66baab1e98e147d84b8a03e922f45e63fa0e
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-09-15 16:01:46 -0300

    add Neighborhood Model

---
+++ app/Models/Neighborhood.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+
+class Neighborhood extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'cityId',
+        'created_at',
+        'updated_at',
+    ];
+
+    public static function getQueryValidator(array $data)
+    {
+        return Validator::make($data, [
+            'id' => ['nullable', 'integer'],
+            'name_search' => ['nullable', 'string'],
+            'cityId' => ['nullable', 'integer'],
+            'created_at_greater_than' => ['nullable', 'date'],
+            'created_at_lesser_than' => ['nullable', 'date'],
+            'updated_at_greater_than' => ['nullable', 'date'],
+            'updated_at_lesser_than' => ['nullable', 'date'],
+        ]);
+    }
+
+    public static function getStoreValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string'],
+            'cityId' => ['required', 'integer', 'exists:cities,id'],
+        ]);
+    }
+
+    public static function getStoreRequestValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string'],
+            'cityId' => ['required', 'integer'],
+        ]);
+    }
+
+    public static function getUpdateValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string'],
+            'cityId' => ['required', 'integer', 'exists:cities,id'],
+        ]);
+    }
+
+    public static function getUpdateRequestValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string'],
+            'cityId' => ['required', 'integer'],
+        ]);
+    }
+}
