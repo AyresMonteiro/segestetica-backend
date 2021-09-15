2147762c609926762f0b6969c24a927d7e9e87d7
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-09-15 14:07:52 -0300

    set up State routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\{
+    StateController,
+};
+use App\Http\Helpers\GenericHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'states'], function () {
+    Route::get('/', GenericHelper::genericTryCatchFactory(StateController::index()));
+    Route::get('/{id}', GenericHelper::genericTryCatchFactory(StateController::show()));
+    Route::post('/', GenericHelper::genericTryCatchFactory(StateController::store()));
+    Route::put('/{id}', GenericHelper::genericTryCatchFactory(StateController::update()));
+    Route::delete('/{id}', GenericHelper::genericTryCatchFactory(StateController::destroy()));
+});
+
