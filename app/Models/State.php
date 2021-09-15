24e663c749b317a6f98d815dd74c98589e4e16e6
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-09-15 14:03:23 -0300

    add State Model

---
+++ app/Models/State.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+
+class State extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'abbreviation',
+        'created_at',
+        'updated_at',
+    ];
+
+    public static function getQueryValidator(array $data)
+    {
+        return Validator::make($data, [
+            'id' => ['nullable', 'integer'],
+            'name' => ['nullable', 'string'],
+            'name_search' => ['nullable', 'string'],
+            'abbreviation' => ['nullable', 'string'],
+            'abbreviation_search' => ['nullable', 'string'],
+            'created_at_greater_than' => ['nullable', 'date'],
+            'created_at_lesser_than' => ['nullable', 'date'],
+            'updated_at_greater_than' => ['nullable', 'date'],
+            'updated_at_lesser_than' => ['nullable', 'date'],
+        ]);
+    }
+
+    public static function getStoreValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string'],
+            'abbreviation' => ['required', 'string'],
+        ]);
+    }
+
+    public static function getStoreRequestValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string'],
+            'abbreviation' => ['required', 'string'],
+        ]);
+    }
+
+    public static function getUpdateValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required_without_all:abbreviation', 'string'],
+            'abbreviation' => ['required_without_all:name', 'string'],
+        ]);
+    }
+
+    public static function getUpdateRequestValidator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required_without_all:abbreviation', 'string'],
+            'abbreviation' => ['required_without_all:name', 'string'],
+        ]);
+    }
+}
