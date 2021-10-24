2e52d43b726024f5f98d316400bf9183ec30b2c0
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-10-24 11:33:52 -0300

    create MailData class

---
+++ app/Http/Handlers/Mail/EmailData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Handlers\Mail;
+
+use App\Exceptions\GenericAppException;
+
+class EmailData
+{
+  public string $name;
+  public string $emailAddress;
+
+  public function __construct($name, $emailAddress)
+  {
+    $this->name = $name;
+    $this->emailAddress = $emailAddress;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function getEmailAddress()
+  {
+    return $this->emailAddress;
+  }
+
+  public function getArray()
+  {
+    return [
+      $this->name,
+      $this->emailAddress
+    ];
+  }
+}
