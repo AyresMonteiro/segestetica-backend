93ecfc390b92468059e888888950a22eecfb6abb
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-10-24 11:38:35 -0300

    setup Establishment's Sanctum Traits

--- app/Models/Establishment.php
+++ app/Models/Establishment.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\HasApiTokens;
 
 class Establishment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $fillable = [
         'uuid',
