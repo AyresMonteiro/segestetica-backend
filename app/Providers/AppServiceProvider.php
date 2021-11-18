9a160dbc097c70a550148a1fdd0a755e76d36b22
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-11-18 15:16:49 -0300

    force https scheme if in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Http\Helpers\GenericHelper;
 use App\Jobs\SendConfirmationMail;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         $this->app->bindMethod([SendConfirmationMail::class, 'handle'], function ($job, $app) {
             $mailHandler = GenericHelper::getDefaultMailHandler();
 
