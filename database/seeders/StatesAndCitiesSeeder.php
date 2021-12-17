66fabd8d7213f62ed96bf538f18cf19ffe339e37
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-17 14:38:16 -0300

    creates StatesAndCitiesSeeder

---
+++ database/seeders/StatesAndCitiesSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Http\Handlers\LogHandler;
+use App\Models\City;
+use App\Models\State;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class StatesAndCitiesSeeder extends Seeder
+{
+    public static function runStatic()
+    {
+        $i = 0;
+        $cities = [];
+        $data = json_decode(file_get_contents(__DIR__ . '/data/estados-e-cidades-do-brasil.json'));
+
+        foreach ($data->estados as $key => $estado) {
+            $state = new State([
+                'name' => $estado->nome,
+                'abbreviation' => $estado->sigla,
+            ]);
+
+            $state->save();
+
+            foreach ($estado->cidades as $key => $cidade) {
+                $city = new City([
+                    'name' => $cidade,
+                    'stateId' => $state->id,
+                ]);
+
+                $city->created_at = Carbon::now();
+                $city->updated_at = Carbon::now();
+                array_push($cities, $city->toArray());
+
+                $i++;
+
+                if ($i == 500) {
+                    City::insert($cities);
+                    $cities = [];
+                    $i = 0;
+                }
+            }
+
+            if ($i > 0) {
+                City::insert($cities);
+                $cities = [];
+                $i = 0;
+            }
+        }
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        self::runStatic();
+    }
+}
