eb558720a8f72771f70832491b368f6ac1d8da8a
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-21 15:35:09 -0300

    replaces GenericHelper::genericTryCatchFactory() with LaravelHTTPRequestAdapter::handle()

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Adapters\LaravelHTTPRequestAdapter;
 use App\Http\Controllers\{
     CityController,
     EstablishmentController,
@@ -7,7 +8,6 @@ use App\Http\Controllers\{
     StateController,
     StreetController,
 };
-use App\Http\Helpers\GenericHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,43 +27,43 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'states'], function () {
-    Route::get('/', GenericHelper::genericTryCatchFactory(StateController::index()));
-    Route::get('/{id}', GenericHelper::genericTryCatchFactory(StateController::show()));
-    Route::post('/', GenericHelper::genericTryCatchFactory(StateController::store()));
-    Route::put('/{id}', GenericHelper::genericTryCatchFactory(StateController::update()));
-    Route::delete('/{id}', GenericHelper::genericTryCatchFactory(StateController::destroy()));
+    Route::get('/', LaravelHTTPRequestAdapter::handle(StateController::index()));
+    Route::get('/{id}', LaravelHTTPRequestAdapter::handle(StateController::show()));
+    // Route::post('/', LaravelHTTPRequestAdapter::handle(StateController::store()));
+    // Route::put('/{id}', LaravelHTTPRequestAdapter::handle(StateController::update()));
+    // Route::delete('/{id}', LaravelHTTPRequestAdapter::handle(StateController::destroy()));
 });
 
 Route::group(['prefix' => 'cities'], function () {
-    Route::get('/', GenericHelper::genericTryCatchFactory(CityController::index()));
-    Route::get('/{id}', GenericHelper::genericTryCatchFactory(CityController::show()));
-    Route::post('/', GenericHelper::genericTryCatchFactory(CityController::store()));
-    Route::put('/{id}', GenericHelper::genericTryCatchFactory(CityController::update()));
-    Route::delete('/{id}', GenericHelper::genericTryCatchFactory(CityController::destroy()));
+    Route::get('/', LaravelHTTPRequestAdapter::handle(CityController::index()));
+    Route::get('/{id}', LaravelHTTPRequestAdapter::handle(CityController::show()));
+    // Route::post('/', LaravelHTTPRequestAdapter::handle(CityController::store()));
+    // Route::put('/{id}', LaravelHTTPRequestAdapter::handle(CityController::update()));
+    // Route::delete('/{id}', LaravelHTTPRequestAdapter::handle(CityController::destroy()));
 });
 
 Route::group(['prefix' => 'neighborhoods'], function () {
-    Route::get('/', GenericHelper::genericTryCatchFactory(NeighborhoodController::index()));
-    Route::get('/{id}', GenericHelper::genericTryCatchFactory(NeighborhoodController::show()));
-    Route::post('/', GenericHelper::genericTryCatchFactory(NeighborhoodController::store()));
-    Route::put('/{id}', GenericHelper::genericTryCatchFactory(NeighborhoodController::update()));
-    Route::delete('/{id}', GenericHelper::genericTryCatchFactory(NeighborhoodController::destroy()));
+    Route::get('/', LaravelHTTPRequestAdapter::handle(NeighborhoodController::index()));
+    Route::get('/{id}', LaravelHTTPRequestAdapter::handle(NeighborhoodController::show()));
+    Route::post('/', LaravelHTTPRequestAdapter::handle(NeighborhoodController::store()));
+    // Route::put('/{id}', LaravelHTTPRequestAdapter::handle(NeighborhoodController::update()));
+    // Route::delete('/{id}', LaravelHTTPRequestAdapter::handle(NeighborhoodController::destroy()));
 });
 
 Route::group(['prefix' => 'streets'], function () {
-    Route::get('/', GenericHelper::genericTryCatchFactory(StreetController::index()));
-    Route::get('/{id}', GenericHelper::genericTryCatchFactory(StreetController::show()));
-    Route::post('/', GenericHelper::genericTryCatchFactory(StreetController::store()));
-    Route::put('/{id}', GenericHelper::genericTryCatchFactory(StreetController::update()));
-    Route::delete('/{id}', GenericHelper::genericTryCatchFactory(StreetController::destroy()));
+    Route::get('/', LaravelHTTPRequestAdapter::handle(StreetController::index()));
+    Route::get('/{id}', LaravelHTTPRequestAdapter::handle(StreetController::show()));
+    Route::post('/', LaravelHTTPRequestAdapter::handle(StreetController::store()));
+    // Route::put('/{id}', LaravelHTTPRequestAdapter::handle(StreetController::update()));
+    // Route::delete('/{id}', LaravelHTTPRequestAdapter::handle(StreetController::destroy()));
 });
 
 Route::group(['prefix' => 'establishments'], function () {
-    Route::get('/', GenericHelper::genericTryCatchFactory(EstablishmentController::index()));
-    Route::get('/confirm', GenericHelper::genericTryCatchFactory(EstablishmentController::confirmEmail()));
-    Route::get('/{uuid}', GenericHelper::genericTryCatchFactory(EstablishmentController::show()));
-    Route::post('/', GenericHelper::genericTryCatchFactory(EstablishmentController::store()));
-    Route::post('/login', GenericHelper::genericTryCatchFactory(EstablishmentController::login()));
-    Route::put('/{uuid}', GenericHelper::genericTryCatchFactory(EstablishmentController::update()));
-    Route::delete('/{uuid}', GenericHelper::genericTryCatchFactory(EstablishmentController::destroy()));
+    Route::get('/', LaravelHTTPRequestAdapter::handle(EstablishmentController::index()));
+    Route::get('/confirm', LaravelHTTPRequestAdapter::handle(EstablishmentController::confirmEmail()));
+    Route::get('/{uuid}', LaravelHTTPRequestAdapter::handle(EstablishmentController::show()));
+    Route::post('/', LaravelHTTPRequestAdapter::handle(EstablishmentController::store()));
+    Route::post('/login', LaravelHTTPRequestAdapter::handle(EstablishmentController::login()));
+    Route::put('/{uuid}', LaravelHTTPRequestAdapter::handle(EstablishmentController::update()));
+    Route::delete('/{uuid}', LaravelHTTPRequestAdapter::handle(EstablishmentController::destroy()));
 });
