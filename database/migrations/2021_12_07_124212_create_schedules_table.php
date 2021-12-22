7c8979347282b75b7cefea0a272f991097cae053
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-22 14:35:54 -0300

    creates schedules table

---
+++ database/migrations/2021_12_07_124212_create_schedules_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->integer('maxServices');
+            $table->time('time');
+            $table->foreignUuid('establishmentUuid');
+            $table->timestamps();
+            $table->boolean('deleted');
+
+            $table
+                ->foreign('establishmentUuid')
+                ->references('uuid')
+                ->on('establishments')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
