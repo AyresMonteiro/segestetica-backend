cb2b577165cce411855e39dc3a7a7ee2f1945603
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-26 19:46:29 -0300

    adds sanctum.no_id message

--- resources/lang/en/sanctum.php
+++ resources/lang/en/sanctum.php
@@ -3,4 +3,5 @@
 return [
   'token_not_sent' => 'Token has not been sent.',
   'invalid_token' => 'Invalid token.',
+  'no_id' => 'There are no id.',
 ];
--- resources/lang/pt-BR/sanctum.php
+++ resources/lang/pt-BR/sanctum.php
@@ -3,4 +3,5 @@
 return [
   'token_not_sent' => 'O token não foi enviado.',
   'invalid_token' => 'Token inválido.',
+  'no_id' => 'Não há id.',
 ];
