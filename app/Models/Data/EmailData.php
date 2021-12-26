c30990586c8a72c145d509c6ad40f3af048eaa57
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-26 08:12:24 -0300

    improves EmailData's syntax

--- app/Models/Data/EmailData.php
+++ app/Models/Data/EmailData.php
@@ -4,13 +4,10 @@ namespace App\Models\Data;
 
 class EmailData
 {
-  public string $name;
-  public string $emailAddress;
-
-  public function __construct($name, $emailAddress)
-  {
-    $this->name = $name;
-    $this->emailAddress = $emailAddress;
+  public function __construct(
+    public string $name,
+    public string $emailAddress
+  ) {
   }
 
   public function getName()
