107cdf7884886ed42ff1a0f5b3ff80f965b7092d
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-26 19:51:35 -0300

    creates AuthenticateEstablishment middleware

---
+++ app/Http/Middleware/AuthenticateEstablishment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\GenericAppException;
+use App\Http\Handlers\ErrorResponseHandler;
+use App\Http\Helpers\GenericHelper;
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+
+class AuthenticateEstablishment
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $authHandler = GenericHelper::getDefaultAuthHandler();
+
+            $bearerData = $authHandler->getBearerData($request);
+
+            $authHandler->eraseCache();
+
+            $tokenData = ['token' => $bearerData->token];
+
+            $authHandler->checkPermission($tokenData, 'establishment:general');
+
+            $request->previousMiddleware = md5(self::class);
+            $request->authData = $authHandler->getSecurityData($tokenData);
+
+            return $next($request);
+        } catch (GenericAppException $e) {
+            return ErrorResponseHandler::customError($e->getCustomErrors(), $e->getStatusCode());
+        } catch (Exception $e) {
+            return ErrorResponseHandler::defaultError();
+        }
+    }
+}
