a073850473b4266e5cf38c6d32319b3494dd3cab
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-12-29 17:19:32 -0300

    asserts Service's getMoneyInfo() operation

--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -22,4 +22,31 @@ class ServiceTest extends TestCase
 
 		$this->assertEquals("19.07", $sut->value);
 	}
+
+	public function test_assertsIfServiceMoneyInfoIsNullWhenModelHasNoData()
+	{
+		$sut = self::makeSut();
+
+		$this->assertEquals(null, $sut->getMoneyInfo());
+	}
+
+	public function test_assertsIfServiceMoneyInfoIsNotNullWhenModelHasData()
+	{
+		$sut = self::makeSut([
+			'integerValue' => 19,
+			'fractionalValue' => 7,
+		]);
+
+		$this->assertNotEquals(null, $sut->getMoneyInfo());
+	}
+
+	public function test_assertsIfServiceMoneyInfoIsAnInstanceOfMoneyDataClass()
+	{
+		$sut = self::makeSut([
+			'integerValue' => 19,
+			'fractionalValue' => 7,
+		]);
+
+		$this->assertEquals(MoneyData::class, $sut->getMoneyInfo()::class);
+	}
 }
