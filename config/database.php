a3636dd889cb242f69b74fbd0ade8e308743d431
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-01-19 03:28:11 -0300

    Omits DATABASE_URL var from sqlite conf

--- config/database.php
+++ config/database.php
@@ -34,11 +34,10 @@ return [
     */
 
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            //'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', storage_path('app/tests/segestetica.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
@@ -123,7 +122,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
