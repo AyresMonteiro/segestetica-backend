603ccee605c7a0bcf99d58e7315af6eaf48f0b11
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-01-20 13:41:10 -0300

    Improves UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Http\Helpers\GenericHelper;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -16,6 +19,16 @@ class UserFactory extends Factory
     protected $model = User::class;
 
     /**
+     * Get a new Faker instance.
+     *
+     * @return \Faker\Generator
+     */
+    public function withFaker()
+    {
+        return \Faker\Factory::create('pt_BR');
+    }
+
+    /**
      * Define the model's default state.
      *
      * @return array
@@ -23,11 +36,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'uuid' => GenericHelper::generateUUIDString(),
+            'name' => $this->faker->firstName(),
+            'lastName' => $this->faker->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'emailConfirmation' => Carbon::now(),
+            'deleted' => false,
+            'phoneNumber' => $this->faker->phoneNumber(),
+            'neighborhoodId' => 1,
+            'passwordHash' => Hash::make('123'),
         ];
     }
 
