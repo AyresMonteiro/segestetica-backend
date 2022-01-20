58eb0cb29a65071ab21d1eec6d053682331e0501
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-01-20 10:33:17 -0300

    Adds HasCompositePrimaryKey trait to EstablishmentService

--- app/Models/EstablishmentService.php
+++ app/Models/EstablishmentService.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\HasCompositePrimaryKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EstablishmentService extends Model
 {
     use HasFactory;
+    use HasCompositePrimaryKey;
+
+    protected $primaryKey = ['establishmentUuid', 'serviceId'];
 
     protected $fillable = [
         'establishmentUuid',
         'serviceId',
+        'active',
     ];
 }
