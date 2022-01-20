95d458b28b72c005409de366219a41f13a64d3df
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-01-20 14:13:17 -0300

    Improves Services' SUT

--- tests/Feature/Services/SUT.php
+++ tests/Feature/Services/SUT.php
@@ -4,18 +4,12 @@ namespace Tests\Feature\Services;
 
 use App\Models\{
 	Establishment,
-	EstablishmentService,
-	Neighborhood,
 	Service,
-	Street
 };
 
 class SUT
 {
 	public Establishment $establishment;
-	public EstablishmentService $establishmentService;
-	public Neighborhood $neighborhood;
 	public Service $service;
-	public Street $street;
 	public String $token;
 }
